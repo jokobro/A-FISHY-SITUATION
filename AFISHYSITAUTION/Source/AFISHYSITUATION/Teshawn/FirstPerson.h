36535912cf1e99e23b0aa287b58d992f669c9b3f
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-11-25 15:00:29 +0100

    basic movement done

---
+++ AFISHYSITAUTION/Source/AFISHYSITUATION/Teshawn/FirstPerson.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FirstPerson.generated.h"
+
+UCLASS()
+class AFISHYSITUATION_API AFirstPerson : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFirstPerson();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+
+	class UCameraComponent* camera;
+
+	void MoveForward(float inputValue);
+	void MoveRight(float inputValue);
+
+	void LookUp(float mouseInput);
+	void Turn(float mouseInput);
+};
